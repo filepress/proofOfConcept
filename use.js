db63fa05d9e724c5186c87ffd6de46ffc1004fcd
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-08-26 16:53:25 +0800

    new structure

---
+++ use.js
@@ -0,0 +1,16 @@
+const filepress = require('./lib/filepress')
+
+filepress('./source')
+	.use(logger)
+
+
+function logger(item) {
+	console.log(item)
+}
+
+/*	.use(frontmatter('yml'))
+	.use(markdown)
+	.use(layouts('js'))
+	.build()
+	.write('./build')
+*/
